0b5391b36e06f0721d8c02506c222e71c020b864
Vern <kuatodev@gmail.com>
2025-03-27 10:45:14 +0700

    Change everything

--- README.md
+++
@@ -1,8 +0,0 @@
-# kuatodev.github.io
-Website copied from iamlooper.github.io :)
-
-## Tools Used
-- [Bootstrap](https://github.com/twbs/bootstrap)
-- [Slick](https://www.jqueryscript.net/slider/Fully-Responsive-Flexible-jQuery-Carousel-Plugin-slick.html)
-- [AOS](https://michalsnik.github.io/aos/)
-- & more...
---
+++ script.js
@@ -0,0 +1,11 @@
+const text = "An Android App Developer";
+let index = 0;
+
+function typeEffect() {
+    document.querySelector(".typing").textContent = text.slice(0, index++);
+    if (index <= text.length) {
+        setTimeout(typeEffect, 100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", typeEffect);
